a97415991afd8c0697d96371b399aabf7002e377
Anastasia-Makarova <140186425+Anastasia-Makarova@users.noreply.github.com>
2024-01-27 00:59:49 +0100

    smth

--- quotes_parser/quotes_parser/spiders/spiders.py
+++ quotes_parser/quotes_parser/spiders/spiders.py
@@ -1,7 +1,8 @@
 import scrapy
 from quotes_parser.items import ScrapyQuoteItem, ScrapyAuthorItem
+import json
 
-authors =[]
+authors = []
 
 
 class QuotesSpider(scrapy.Spider):
@@ -14,6 +15,7 @@ class QuotesSpider(scrapy.Spider):
         quote = ScrapyQuoteItem()
         quote['tags'] = raw_quote.xpath("div[@class='tags']/a/text()").getall()
         quote['author'] = raw_quote.xpath("span/small[@class='author']/text()").get()
+        # quote['link'] = raw_quote.xpath("span[2]/a/@href").get() 
         quote['quote'] = raw_quote.xpath("span[@class='text']/text()").get()
 
         return quote
@@ -22,19 +24,28 @@ class QuotesSpider(scrapy.Spider):
         quotes = response.xpath("/html//div[@class='quote']")
 
         for quote in quotes:
+            # authors.append(quote.xpath("span[2]/a/@href").get())
+            link =f'https://quotes.toscrape.com{quote.xpath("span[2]/a/@href").get()}'
+            if link not in authors:
+                authors.append(link)
             yield self.compose_quote(quote)
-            
+ 
         next_page = response.xpath("//li[@class='next']/a/@href").get()
 
         if next_page is not None:
             yield scrapy.Request(url=self.start_urls[0] + next_page)
+
+        with  open ('authors_links.json', 'a') as file:
+                    json.dump(authors, file)
+
+
 
 
 
 class AuthorsSpider(scrapy.Spider):
     name = "authors"
     allowed_domains = ["quotes.toscrape.com"]
-    start_urls = ["https://quotes.toscrape.com"]
+    start_urls = authors
 
     def compose_author(self, raw_author):
 
@@ -46,8 +57,9 @@ class AuthorsSpider(scrapy.Spider):
 
         return author
 
-    def parse(self):
+    def parse(self, response):
         for author in authors:
-            yield self.compose_author(author)
+            url = f"https://quotes.toscrape.com{author}"
+            yield self.compose_author(url)
 
    
